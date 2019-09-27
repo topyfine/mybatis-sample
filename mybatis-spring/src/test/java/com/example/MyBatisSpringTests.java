21c387dae78e5d861286d41f733c40096224e3ef
topyfine <young_fine@163.com>
2019-09-27 23:32:47 +0800

    transaction manager

--- mybatis-spring/src/test/java/com/example/MyBatisSpringTests.java
+++ mybatis-spring/src/test/java/com/example/MyBatisSpringTests.java
@@ -3,8 +3,11 @@ package com.example;
 import com.example.dao.User;
 import com.example.dao.UserMapper;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
@@ -32,4 +35,21 @@ public class MyBatisSpringTests {
         assertNotNull(users);
     }
 
+//    @Test
+//    public void testMapper2() {
+//        SqlSessionFactory sqlSessionFactory = applicationContext.getBean(SqlSessionFactory.class);
+//        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
+//        UserMapper mapper = sqlSessionTemplate.getMapper(UserMapper.class);
+//        assertNotNull(mapper);
+//        mapper.getUsers().forEach(System.out::println);
+//    }
+
+//    @Test
+//    public void testMapper3() throws Exception {
+//        SqlSessionFactory sqlSessionFactory = applicationContext.getBean(SqlSessionFactory.class);
+//        MapperFactoryBean<UserMapper> factoryBean = new MapperFactoryBean<>(UserMapper.class);
+//        factoryBean.setSqlSessionFactory(sqlSessionFactory);
+//        UserMapper userMapper = factoryBean.getObject();
+//        assertNotNull(userMapper);
+//    }
 }
